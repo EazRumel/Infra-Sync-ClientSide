66e4eeb92a3a2aa1600546c570fe3fd4e9bd3121
Hemel <reaz.hemel@gmail.com>
2025-02-03 18:39:03 +0600

    updated the rooms fetch from the server side

--- src/assets/components/ApartmentCard.jsx
+++ src/assets/components/ApartmentCard.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
-
 const ApartmentCard = ({apartment}) => {
 
   return (
-    <div className="card bg-base-100 image-full  shadow-sm">
-  <figure>
+//     <div className="card bg-base-100 image-full  shadow-sm">
+//   <figure>
+//     <img
+//       src={apartment.apartment_image}
+//       alt="Shoes" />
+//   </figure>
+//   <div className="card-body">
+//     <h2 className="card-title">{apartment.apartment_no}</h2>
+//     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+//     <div className="card-actions justify-end">
+//       <button className="btn bg-teal-500">Agreement</button>
+//     </div>
+//   </div>
+// </div>
+<div className="card bg-base-100  shadow-sm">
+  <figure className="px-10 pt-10">
     <img
       src={apartment.apartment_image}
-      alt="Shoes" />
+      alt="Shoes"
+      className="rounded-xl w-full" />
   </figure>
-  <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">Apartment Number:-{apartment.apartment_no}</h2>
+    <p>Floor Number:{apartment.floor_no}</p>
+    <p>Block Number:{apartment.block_name}</p>
+    <div className="card-actions">
+      <button className="btn bg-teal-500 hover:bg-teal-300 text-white">Agreement</button>
     </div>
   </div>
 </div>
--- src/pages/Apartment.jsx
+++ src/pages/Apartment.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import ApartmentCard from "../assets/components/ApartmentCard";
+import { Helmet } from "react-helmet-async";
 
 
 const Apartment = () => {
@@ -14,11 +15,14 @@ const Apartment = () => {
   },[])
   return (
     <div>
+    <Helmet>
+      <title>Apartment || IS</title>
+    </Helmet>
     <h2 className="text-4xl text-center mt-10 text-teal-500 mb-10">Apartment</h2>
     <hr className="text-teal-500" />
       <div className="my-10 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-4 mx-10">
        {
-        apartments.map(apartment => <ApartmentCard key={apartment.id} apartment={apartment}></ApartmentCard>)
+        apartments.map(apartment => <ApartmentCard key={apartment._id} apartment={apartment}></ApartmentCard>)
        }
     </div>
     </div>
